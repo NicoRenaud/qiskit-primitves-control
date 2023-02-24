85d640443bc799372007078af8b9266e4f72de28
Nicolas Renaud <nicolas.gm.renaud@gmail.com>
2023-02-24 15:47:29 +0100

    add new circuit

--- session_works.py
+++ session_works.py
@@ -5,27 +5,28 @@ import numpy as np
 from qiskit import QuantumCircuit
 from qiskit.quantum_info import SparsePauliOp
 
-# create subcircuits
 qc0 = random_circuit(2, 2, seed=1).decompose(reps=1)
 qc1 = random_circuit(2, 2, seed=1).decompose(reps=1)
 
-# compose them with a control
-qc = QuantumCircuit(3)
-qc.compose(qc0.control(1), [0,1,2], inplace=True)
-qc.compose(qc1.control(1), [0,1,2], inplace=True)
+qc01 = QuantumCircuit(2)
+qc01.compose(qc0,[0,1],inplace=True)
+qc01.compose(qc1,[0,1],inplace=True)
 
-# create observable
+qc = QuantumCircuit(3)
+qc.compose(qc01.control(1),[0,1,2], inplace=True)
+
 obs = SparsePauliOp(["IIX","IIZ"], np.array([0.5, -0.5]))
 
-# init runtim
 service = QiskitRuntimeService()
 backend =  "simulator_statevector"
 # backend = "ibmq_belem" 
 # backend =  "ibmq_qasm_simulator"
 
-# execute in a session
 with Session(service=service, backend=backend) as session:
     estimator = Estimator(session=session)
+    
     job = estimator.run(qc, obs)
     result = job.result()
     session.close()
+
+print(result)
